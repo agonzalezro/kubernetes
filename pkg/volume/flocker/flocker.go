266133cce0e772de3f00528c0cdff9145aae6721
Álex González <agonzalezro@gmail.com>
2015-09-17 15:08:59 +0100

    Create volume & wait for it to be ready

--- pkg/volume/flocker/flocker.go
+++ pkg/volume/flocker/flocker.go
@@ -3,7 +3,9 @@ package flocker
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/types"
@@ -11,9 +13,6 @@ import (
 	"k8s.io/kubernetes/pkg/util/mount"
 	"k8s.io/kubernetes/pkg/volume"
 )
-
-// TODO: From https://github.com/ClusterHQ/flocker-docker-plugin/blob/master/flockerdockerplugin/adapter.py#L18
-const defaultVolumeSize = 107374182400
 
 func ProbeVolumePlugins() []volume.VolumePlugin {
 	return []volume.VolumePlugin{&flockerPlugin{}}
@@ -82,8 +81,8 @@ func (b flockerBuilder) SetUp() error {
 }
 
 func (b flockerBuilder) SetUpAt(dir string) error {
-	c := newFlockerClient()
-	return c.createVolume(string(b.pod.UID), dir)
+	c := newFlockerClient(b.pod)
+	return c.createVolume(dir)
 }
 
 func (b flockerBuilder) IsReadOnly() bool {
@@ -92,12 +91,44 @@ func (b flockerBuilder) IsReadOnly() bool {
 
 // TODO: -- CUT HERE --
 
+const (
+	controlHost = "172.16.255.250"
+	controlPort = 4523
+
+	// TODO: From https://github.com/ClusterHQ/flocker-docker-plugin/blob/master/flockerdockerplugin/adapter.py#L18
+	defaultVolumeSize = 107374182400
+
+	keyFile  = "/etc/flocker/api.key"
+	certFile = "/etc/flocker/api.crt"
+	caFile   = "/etc/flocker/cluster.crt"
+)
+
 type flockerClient struct {
 	*http.Client
+
+	pod *api.Pod
+
+	host    string
+	port    int
+	version string
+
+	maximumSize int
+
+	ca, key, cert string // kubelet hardcoded paths
 }
 
-func newFlockerClient() *flockerClient {
-	return &flockerClient{&http.Client{}}
+func newFlockerClient(pod *api.Pod) *flockerClient {
+	return &flockerClient{
+		Client:      &http.Client{},
+		pod:         pod,
+		host:        controlHost,
+		port:        controlPort,
+		version:     "v1",
+		maximumSize: defaultVolumeSize,
+		ca:          caFile,
+		key:         keyFile,
+		cert:        certFile,
+	}
 }
 
 func (c flockerClient) post(url string, payload interface{}) (*http.Response, error) {
@@ -116,22 +147,73 @@ func (c flockerClient) post(url string, payload interface{}) (*http.Response, er
 	return c.Do(req)
 }
 
-func (c flockerClient) createVolume(primary, datasetID string) error {
+func (c flockerClient) getURL(path string) string {
+	return fmt.Sprintf("https://%s:%d/%s/%s", c.host, c.port, c.version, path)
+}
+
+func (c flockerClient) checkVolumeErr(datasetID string) error {
 	payload := struct {
-		Primary   string `json:"primary"`
-		DatasetID string `json:"dataset_id"`
+		Primary string `json:"primary"`
 	}{
-		primary,
-		datasetID,
+		string(c.pod.UID),
 	}
 
-	resp, err := c.post(URL_HERE, payload)
+	resp, err := c.post(c.getURL(fmt.Sprintf("configuration/datasets/%s", datasetID)), payload)
 	if err != nil {
 		resp.Body.Close()
 		return err
 	}
 	defer resp.Body.Close()
-	// READ JSON RESPONSE FROM FLOCKER
+
+	status := resp.StatusCode
+	if status < 200 || status > 299 {
+		return fmt.Errorf("Expected: 2xx getting the volume, got: %d", status)
+	}
+	return nil
+}
+
+func (c flockerClient) createVolume(datasetID string) error {
+	// TODO: probably, datasetID needs to be clean, ex: remove /
+	payload := struct {
+		Primary     string `json:"primary"`
+		DatasetID   string `json:"dataset_id"`
+		MaximumSize string `json:"maximum_size"`
+	}{
+		string(c.pod.UID),
+		datasetID,
+		c.MaximumSize,
+	}
+
+	// TODO: we could create 2 goroutines for create & try to get at the same time
+	resp, err := c.post(c.getURL("configuration/datasets"), payload)
+	if err != nil {
+		resp.Body.Close()
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 {
+		return fmt.Errorf("Expected: {1,2}xx creating the volume, got: %d", resp.StatusCode)
+	}
+
+	// Wait until the dataset is ready for usage, this can take a long a time
+	if err := c.checkVolumeErr(datasetID); err != nil {
+		// TODO: 30 is a magic number to wait for the volume
+		timeoutChan := time.NewTimer(30 * time.Second).C
+		tickChan := time.NewTicker(500 * time.Millisecond).C
+
+		for {
+			select {
+			case <-timeoutChan:
+				// TODO: a goroutine can be running at this point, but it's not a big deal
+				return err
+			case <-tickChan:
+				if err := c.checkVolumeErr(datasetID); err == nil {
+					return nil
+				}
+			}
+		}
+	}
 
 	return nil
 }
