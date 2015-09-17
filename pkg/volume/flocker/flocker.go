04ba4aa2023d942084110041d0caafd6b0aac954
Álex González <agonzalezro@gmail.com>
2015-09-17 18:02:39 +0100

    Creating volumes and returning paths

--- pkg/volume/flocker/flocker.go
+++ pkg/volume/flocker/flocker.go
@@ -3,7 +3,9 @@ package flocker
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -82,7 +84,9 @@ func (b flockerBuilder) SetUp() error {
 
 func (b flockerBuilder) SetUpAt(dir string) error {
 	c := newFlockerClient(b.pod)
-	return c.createVolume(dir)
+	// TODO: the _ is the path
+	_, err := c.createVolume(dir)
+	return err
 }
 
 func (b flockerBuilder) IsReadOnly() bool {
@@ -131,13 +135,20 @@ func newFlockerClient(pod *api.Pod) *flockerClient {
 	}
 }
 
-func (c flockerClient) post(url string, payload interface{}) (*http.Response, error) {
-	b, err := json.Marshal(payload)
-	if err != nil {
-		return nil, err
+func (c flockerClient) request(method, url string, payload interface{}) (*http.Response, error) {
+	var (
+		b   []byte
+		err error
+	)
+
+	if method == "POST" { // Just allow payload on POST
+		b, err = json.Marshal(payload)
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(b))
 	if err != nil {
 		return nil, err
 	}
@@ -145,6 +156,14 @@ func (c flockerClient) post(url string, payload interface{}) (*http.Response, er
 
 	// REMEMBER TO CLOSE THE BODY IN THE OUTSIDE FUNCTION
 	return c.Do(req)
+}
+
+func (c flockerClient) post(url string, payload interface{}) (*http.Response, error) {
+	return c.request("POST", url, payload)
+}
+
+func (c flockerClient) get(url string) (*http.Response, error) {
+	return c.request("GET", url, nil)
 }
 
 func (c flockerClient) getURL(path string) string {
@@ -172,32 +191,125 @@ func (c flockerClient) checkVolumeErr(datasetID string) error {
 	return nil
 }
 
-func (c flockerClient) createVolume(datasetID string) error {
-	// TODO: probably, datasetID needs to be clean, ex: remove /
-	payload := struct {
-		Primary     string `json:"primary"`
-		DatasetID   string `json:"dataset_id"`
-		MaximumSize string `json:"maximum_size"`
-	}{
-		string(c.pod.UID),
-		datasetID,
-		c.MaximumSize,
-	}
+type configurationPayload struct {
+	Primary     string          `json:"primary"`
+	DatasetID   string          `json:"dataset_id"`
+	MaximumSize int             `json:"maximum_size"`
+	Metadata    metadataPayload `json:"metadata"`
+}
 
-	// TODO: we could create 2 goroutines for create & try to get at the same time
-	resp, err := c.post(c.getURL("configuration/datasets"), payload)
+type metadataPayload struct {
+	Name string `json:"name"`
+}
+
+type state struct {
+	Path      string `json:"path"`
+	DatasetID string `json:"dataset_id"`
+}
+
+type statePayload struct {
+	*state
+}
+
+// OMG, look at that name
+func (c flockerClient) findDatasetIDByNameInConfigurationsPayload(body io.ReadCloser, name string) (dataset_id string, err error) {
+	var result []configurationPayload
+	if err := json.NewDecoder(body).Decode(&result); err != nil {
+		for _, r := range result {
+			if r.Metadata.Name == name {
+				return r.DatasetID, nil
+			}
+		}
+	}
+	return "", errors.New("Configuration not found by Name")
+}
+
+// OMG DITTO
+func (c flockerClient) findStateByDatasetIDInStatesPayload(body io.ReadCloser, datasetID string) (path string, err error) {
+	var result []statePayload
+	if err = json.NewDecoder(body).Decode(&result); err != nil {
+		for _, r := range result {
+			if r.DatasetID == datasetID {
+				return r.Path, nil
+			}
+		}
+		return "", errors.New("State not found by Dataset ID")
+	}
+	return "", err
+}
+
+func (c flockerClient) getState(datasetID string) (*state, error) {
+	resp, err := c.get(c.getURL("state/datasets"))
 	if err != nil {
 		resp.Body.Close()
-		return err
+		return nil, err
+	}
+
+	path, err := c.findStateByDatasetIDInStatesPayload(resp.Body, datasetID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &state{
+		DatasetID: datasetID,
+		Path:      path,
+	}, nil
+}
+
+/*
+ * I feel a little bit guilty with this func, it should be refactored & tested later:
+ *
+ * 1) Get all the datasets
+ * 2) If a dataset with that name/dir is found, return its path
+ * 3) If not, create a new one
+ * 4) Wait until the dataset is ready
+ */
+func (c flockerClient) createVolume(dir string) (path string, err error) {
+	// TODO: probably, datasetID needs to be clean, ex: remove /
+	payload := configurationPayload{
+		Primary:     string(c.pod.UID),
+		MaximumSize: defaultVolumeSize,
+		Metadata: metadataPayload{
+			Name: dir,
+		},
+	}
+
+	// 1 & 2) Try to find the dataset if it was previously created
+	resp, err := c.get(c.getURL("configuration/datasets"))
+	if err != nil {
+		resp.Body.Close()
+		return "", err
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode >= 300 {
-		return fmt.Errorf("Expected: {1,2}xx creating the volume, got: %d", resp.StatusCode)
+	if datasetID, err := c.findDatasetIDByNameInConfigurationsPayload(resp.Body, dir); err == nil {
+		state, err := c.getState(datasetID)
+		if err != nil {
+			return "", err
+		}
+		return state.Path, nil
 	}
 
-	// Wait until the dataset is ready for usage, this can take a long a time
-	if err := c.checkVolumeErr(datasetID); err != nil {
+	// 3) Create a new one if we get here
+	resp, err = c.post(c.getURL("configuration/datasets"), payload)
+	if err != nil {
+		resp.Body.Close()
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 300 { // TODO: possible 409 race condition
+		return "", fmt.Errorf("Expected: {1,2}xx creating the volume, got: %d", resp.StatusCode)
+	}
+
+	// TODO: not sure if reusing payload is a good idea, let's see
+	if err := json.NewDecoder(resp.Body).Decode(&payload); err != nil {
+		return "", nil
+	}
+	datasetID := payload.DatasetID
+
+	// 4) Wait until the dataset is ready for usage, this can take a long a time
+	if state, err := c.getState(datasetID); err != nil {
 		// TODO: 30 is a magic number to wait for the volume
 		timeoutChan := time.NewTimer(30 * time.Second).C
 		tickChan := time.NewTicker(500 * time.Millisecond).C
@@ -206,14 +318,14 @@ func (c flockerClient) createVolume(datasetID string) error {
 			select {
 			case <-timeoutChan:
 				// TODO: a goroutine can be running at this point, but it's not a big deal
-				return err
+				return "", err
 			case <-tickChan:
-				if err := c.checkVolumeErr(datasetID); err == nil {
-					return nil
+				if state, err := c.getState(datasetID); err == nil {
+					return state.Path, nil
 				}
 			}
 		}
+	} else {
+		return state.Path, nil
 	}
-
-	return nil
 }
